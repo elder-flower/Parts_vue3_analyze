6ac90f36b4c2d063775db7bb2f599b3381d4cd8b
elder-flower <flowercrown10@gmail.com>
2022-03-11 14:50:34 +0900

    Init

--- js2/gmenu.js
+++ js2/gmenu.js
@@ -1,58 +1,15 @@
-var doc = document;
-var glinks = doc.querySelectorAll('.g-nav a');
-var $win = $(window);
-var $obtn = $('.openbtn');
-var $gnav = $('.g-nav');
-var $html = $('html');
+const win = window;
+const $obtn = $(".openbtn");
 
-function sanitize() {
-  sanitize_core(glinks);
+// ページが読み込まれたらすぐに動かしたい場合の記述
 
-  function sanitize_core(elems) {
-    if (elems == null) {
-      return;
-    }
-
-    var len = elems.length;
-
-    if (len) {
-      for (var zz = 0; zz < len; zz++) {
-        var elem = elems[zz];
-        if (elem.hasAttribute("href")) {
-          var link = elem.getAttribute("href");
-          elem.removeAttribute("href");
-          elem.setAttribute("data-href", link);
-        }
-      }
-    } else {
-      if (elem.hasAttribute("href")) {
-        var link = elems.getAttribute("href");
-        elems.removeAttribute("href");
-        elems.setAttribute("data-href", link);
-      }
-    }
-  }
-}
+win.addEventListener("load", openBtn, false);
 function openBtn() {
-  $obtn[0].addEventListener("click", gmenu, false);
+  $obtn.each(function (i, elem) {
+    elem.addEventListener("click", gmenu, false);
+  });
+}
 
-  for (var aa = 0; aa < glinks.length; aa++) {
-    var glink = glinks[aa];
-    glink.addEventListener("click", gmenu2, {
-      passive: true,
-    });
-  }
-}
 function gmenu() {
-  $obtn.toggleClass("active"); //ボタン自身に activeクラスを付与し
-  $html.toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+  $obtn.toggleClass("active");
 }
-function gmenu2() {
-  $obtn.removeClass("active"); //ボタンの activeクラスを除去し
-  $html.removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
-}
-function Init() {
-  sanitize();
-  openBtn();
-}
-$win[0].addEventListener("load", Init, false);
