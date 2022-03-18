9f5b526b5ef432d64d2997fb7b244331aec8e623
elder-flower <flowercrown10@gmail.com>
2022-03-18 23:13:18 +0900

    Init

--- js2/set.js
+++ js2/set.js
@@ -1,51 +1,47 @@
 var doc = document;
-var totop = doc.querySelector("#page-top a");
+
+var togo = doc.querySelector(".js-scroll a");
+var totop = doc.querySelector(".js-pagetop a");
+
 var $win = $(window);
-var $ptop = $("#page-top");
-var $footer = $("#footer");
+var $js_scroll = $(".js-scroll");
+var $js_ptop = $(".js-pagetop");
+var $bdy = $("body,html");
 
 //スクロールした際の動きを関数でまとめる
-function PageTopAnime() {
-  var scroll = $win.scrollTop(); //スクロール値を取得
-  if (scroll >= 200) {
-    //200pxスクロールしたら
-    $ptop.removeClass("DownMove"); // DownMoveというクラス名を除去して
-    $ptop.addClass("UpMove"); // UpMoveというクラス名を追加して出現
-  } else {
-    //それ以外は
-    if ($ptop.hasClass("UpMove")) {
-      //UpMoveというクラス名が既に付与されていたら
-      $ptop.removeClass("UpMove"); //  UpMoveというクラス名を除去し
-      $ptop.addClass("DownMove"); // DownMoveというクラス名を追加して非表示
-    }
-  }
+function PageTopCheck() {
+  var winScrollTop = $win.scrollTop();
+  var secondTop = $("#area-2").offset().top - 150; //#area-2の上から150pxの位置まで来たら
 
-  var wH = window.innerHeight; //画面の高さを取得
-  var footerPos = $footer.offset().top; //footerの位置を取得
-  if (scroll + wH >= footerPos + 10) {
-    var pos = scroll + wH - footerPos + 10; //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
-    $ptop.css("bottom", pos); //#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
+  if (winScrollTop >= secondTop) {
+    $js_scroll.removeClass("scroll-view"); //.js-scrollからscroll-viewというクラス名を除去
+    $js_ptop.addClass("scroll-view"); //.js-pagetopにscroll-viewというクラス名を付与
   } else {
-    //それ以外は
-    if ($ptop.hasClass("UpMove")) {
-      //UpMoveというクラス名がついていたら
-      $ptop.css("bottom", "10px"); // 下から10pxの位置にページリンクを指定
-    }
+    //元に戻ったら
+    $js_scroll.addClass("scroll-view"); //.js-scrollからscroll-viewというクラス名を付与
+    $js_ptop.removeClass("scroll-view"); //.js-pagetopからscroll-viewというクラス名を除去
   }
 }
-function Set() {
-  totop.addEventListener("click", goto, false);
 
-  function goto() {
-    $("body,html").animate(
-      {
-        scrollTop: 0,
-      },
-      500
-    );
+function Set() {
+  togo.addEventListener("click", goto, false);
+
+  totop.addEventListener("click", topto, false);
+
+  function goto(e) {
+    var tar = e.target;
+
+    var elmHash = tar.getAttribute("data-href"); //hrefの内容を取得
+
+    var pos = $(elmHash).offset().top;
+    $bdy.animate({ scrollTop: pos }, pos); //#area-2にスクロール
+  }
+  function topto() {
+    $bdy.animate({ scrollTop: 0 }, 500); //それ以外はトップへスクロール。数字が大きくなるほどゆっくりスクロール
   }
 }
 function sanitize() {
+  sanitize_core(togo);
   sanitize_core(totop);
 
   function sanitize_core(elems) {
@@ -75,8 +71,8 @@ function sanitize() {
 }
 function Init() {
   sanitize();
-  PageTopAnime();
+  PageTopCheck();
   Set();
 }
-$win[0].addEventListener("scroll", PageTopAnime, false);
+$win[0].addEventListener("scroll", PageTopCheck, false);
 $win[0].addEventListener("DOMContentLoaded", Init, false);
