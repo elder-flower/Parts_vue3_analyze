6595e6b3014c992484558a1469fac64676ff51fe
elder-flower <flowercrown10@gmail.com>
2022-03-18 13:23:32 +0900

    Init2

--- js2/set.js
+++ js2/set.js
@@ -1,46 +1,29 @@
-var win = window;
-var doc = document;
 
-var obtns = doc.getElementsByClassName("open-btn");
-var obtns_len = obtns.length;
+var $win = $(window);
+var obtn = '.open-btn';
+var pac = 'panelactive';
+var ac = 'btnactive';
+var cls = '#search-wrap';
 
-function Set() {
-  for (var aa = obtns_len; aa--; ) {
-    var obtn = obtns[aa];
-    obtn.addEventListener("click", exe, false);
-  }
-}
 
-var pac = "panelactive";
-var bac = "btnactive";
-var swrap = doc.getElementById("search-wrap");
-var stext = doc.getElementById("search-text");
+function Set(){
 
-function exe() {
-  //.open-btnは、クリックごとにbtnactiveクラスを付与＆除去。1回目のクリック時は付与
-  for (var bb = obtns_len; bb--; ) {
-    var obtn = obtns[bb];
-    obtn.classList.toggle(bac);
+    var type = 'click';
+    $obtn = $( obtn );
 
-    var cls_lists = [...obtn.classList];
-    //console.log( cls_lists );
+    //開閉ボタンを押した時には
+    $obtn.on( type, function(){
 
-    var flag = cls_lists.find((elm) => elm === bac);
-    //console.log( flag );
+        $obtn.toggleClass( ac );//.open-btnは、クリックごとにbtnactiveクラスを付与＆除去。1回目のクリック時は付与
+        $( cls ).toggleClass(pac);//#search-wrapへpanelactiveクラスを付与
+        $('#search-text').focus();//テキスト入力のinputにフォーカス
 
-    if (flag === bac) {
-      obtn.setAttribute("aria-pressed", true);
-    }else{
-      obtn.setAttribute("aria-pressed", false);
-    }
-  }
+        var flag = $obtn.hasClass( ac );
+        $obtn[0].setAttribute('aria-pressed', flag);
 
-  //#search-wrapへpanelactiveクラスを付与
-  swrap.classList.toggle(pac);
+    });
 
-  //テキスト入力のinputにフォーカス
-  stext.focus();
 }
 
 // ページが読み込まれたらすぐに動かしたい場合の記述
-win.addEventListener("DOMContentLoaded", Set, false);
+$win[0].addEventListener( 'DOMContentLoaded', Set, false );
