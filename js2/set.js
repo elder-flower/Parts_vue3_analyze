5e96e2f3dbdca4915423dea6f45c6a521b5108dd
elder-flower <flowercrown10@gmail.com>
2022-03-18 13:22:35 +0900

    Init

--- js2/set.js
+++ js2/set.js
@@ -4,34 +4,42 @@ var doc = document;
 var obtns = doc.getElementsByClassName("open-btn");
 var obtns_len = obtns.length;
 
-var clbtns = doc.getElementsByClassName("close-btn");
-var clbtns_len = clbtns.length;
-
-var swrap = doc.getElementById("search-wrap");
-var stext = doc.getElementById("search-text");
-
-var pa_class_name = "panelactive";
-
 function Set() {
   for (var aa = obtns_len; aa--; ) {
     var obtn = obtns[aa];
-    obtn.addEventListener("click", addClass, false);
-  }
-  for (var bb = clbtns_len; bb--; ) {
-    var clbtn = clbtns[bb];
-    clbtn.addEventListener("click", removeClass, false);
+    obtn.addEventListener("click", exe, false);
   }
 }
 
-function addClass() {
-  //IE11では使えないっぽい。
-  swrap.classList.add(pa_class_name); //#search-wrapへpanelactiveクラスを付与
-  stext.focus(); //テキスト入力のinputにフォーカス
-}
+var pac = "panelactive";
+var bac = "btnactive";
+var swrap = doc.getElementById("search-wrap");
+var stext = doc.getElementById("search-text");
 
-function removeClass() {
-  //IE11では使えないっぽい。
-  swrap.classList.remove(pa_class_name); //#search-wrapからpanelactiveクラスを除去
+function exe() {
+  //.open-btnは、クリックごとにbtnactiveクラスを付与＆除去。1回目のクリック時は付与
+  for (var bb = obtns_len; bb--; ) {
+    var obtn = obtns[bb];
+    obtn.classList.toggle(bac);
+
+    var cls_lists = [...obtn.classList];
+    //console.log( cls_lists );
+
+    var flag = cls_lists.find((elm) => elm === bac);
+    //console.log( flag );
+
+    if (flag === bac) {
+      obtn.setAttribute("aria-pressed", true);
+    }else{
+      obtn.setAttribute("aria-pressed", false);
+    }
+  }
+
+  //#search-wrapへpanelactiveクラスを付与
+  swrap.classList.toggle(pac);
+
+  //テキスト入力のinputにフォーカス
+  stext.focus();
 }
 
 // ページが読み込まれたらすぐに動かしたい場合の記述
