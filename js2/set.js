7927391f5f0a59868e99a91e996430002f4fd5d4
elder-flower <flowercrown10@gmail.com>
2022-03-18 18:30:46 +0900

    Init

--- js2/set.js
+++ js2/set.js
@@ -1,18 +1,14 @@
-$(window).on('load',function(){
-    $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
-	
-    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-    $("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
-    
-        $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
-	
+$(window).on("load", function () {
+  $("#splash")
+    .delay(1500)
+    .fadeOut("slow", function () {
+      //ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+      $("body").addClass("appear"); //フェードアウト後bodyにappearクラス付与
+      var h = $(window).height(); //ブラウザの高さを取得
+      $(".splashbg").css({
+        "border-width": h, //ボーダーの太さにブラウザの高さを代入
+        "animation-name": "backBoxAnime", //animation-nameを定義
+      });
     });
-    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-    
-   //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
-    $('.splashbg').on('animationend', function() {    
-        //この中に動かしたいJSを記載
-    });
-    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
-        
+  $("#splash-logo").delay(1200).fadeOut("slow"); //ロゴを1.2秒でフェードアウトする記述
 });
