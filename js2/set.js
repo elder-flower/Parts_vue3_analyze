4a27611e4f7d5903f6a2995e89836aff541e2360
elder-flower <flowercrown10@gmail.com>
2022-03-19 19:34:31 +0900

    Init

--- js2/set.js
+++ js2/set.js
@@ -1,5 +1,41 @@
-$("#container").scrollgress({
-  //バーの高さの基準となるエリア指定
-  height: "5px", //バーの高さ
-  color: "#eeeeee", //バーの色
+
+var $win = $(window);
+
+//線が伸びるための設定を関数でまとめる
+function ScrollTimelineAnime(){
+
+    $('.timeline li').each(function(){// それぞれのli要素の
+
+        var $th = $(this);
+        var elemPos = $th.offset().top;// 上からの高さ取得
+        var scroll = $win.scrollTop();// スクロール値取得
+        var windowHeight = $win.height();// windowの高さ取得
+        var startPoint = 100; //線をスタートさせる位置を指定※レイアウトによって調整してください
+
+        if (scroll >= elemPos - windowHeight-startPoint){
+            var H = $th.outerHeight(true)//liの余白と高さを含めた数値を取得
+            //スクロール値から要素までの高さを引いた値を、liの高さの半分のパーセントで出す
+            var percent = (scroll+startPoint - elemPos) / (H/2) *100;//liの余白と高さの半分で線を100％に伸ばす
+
+            // 100% を超えたらずっと100%を入れ続ける
+            if(percent  > 100){
+                percent  = 100;
+            }
+            // ボーダーの長さをセット
+            $th.children('.border-line').css({
+                height: percent + "%", //CSSでパーセント指定
+            });
+        } 
+    });
+}
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$win.on('scroll', function(){
+    ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$win.on('load', function(){
+    ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
 });
