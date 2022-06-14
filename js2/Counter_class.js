92c0c2404294f6808105b9ae683f1a517ddb9a36
elder-flower <flowercrown10@gmail.com>
2022-06-14 16:26:12 +0900

    Counter Class 更新

--- js2/Counter_class.js
+++ js2/Counter_class.js
@@ -2,6 +2,9 @@
  * 変更履歴。
  * 001 「終了後に実行する関数」を追加する機能を追加。
  * 002 「progress要素」にも対応。
+ * 003 スクロール中に関数が再発動する事があり、到達点の数字が変わってしまうことがあるので「counter2()」を追加。
+ * 004 プログレスバーのみ繰り返し動かすメソッドを追加。
+ * 005 desktop Safariの不具合対応。
  */
 /**
  * 受け取った要素の中にある文字列の数字をカウントする関数。
@@ -10,6 +13,7 @@
 export class Counter_Class {
   constructor({
     elem,
+    attr,
     duration,
     countdown,
     endFunc,
@@ -17,10 +21,12 @@ export class Counter_Class {
     win,
     progressElem,
     progressDiv,
+    progress_bar_count_repeat,
   }) {
     this.num = 0;
     this.startTime = 0;
     this.elem = elem;
+    this.attr = attr != null ? attr : 'data-num';
     this.duration = duration != null ? duration : 1000;
     this.countDown = countdown;
     this.easing = easing != null ? easing : this.linear;
@@ -29,6 +35,9 @@ export class Counter_Class {
     this.endFunc = endFunc;
     this.progressElem = progressElem;
     this.progressDiv = progressDiv;
+    this.Running = false;
+    this.progress_bar_count_repeat =
+      progress_bar_count_repeat != null ? progress_bar_count_repeat : false;
   }
   /**
    * アニメーションのイージング関数です
@@ -40,12 +49,6 @@ export class Counter_Class {
   }
   easeOutQuart(x) {
     return 1 - Math.pow(1 - x, 4);
-  }
-  counter() {
-    const num = this.elem.innerHTML * 1;
-    this.num = num;
-    this.startTime = performance.now();
-    this.count(this.startTime);
   }
   count(time) {
     const progress = (time - this.startTime) / this.duration;
@@ -65,15 +68,86 @@ export class Counter_Class {
         this.progressElem.innerHTML = num2_;
       } else {
         if (this.progressDiv !== undefined) {
+          /*
+          「scaleによる伸縮」がdesktop Safariで出来ないので。
           this.progressDiv.style.transformOrigin = 'left center';
           this.progressDiv.style.transform = `translate(-50%, -50%) scale(${num2_}%, 100%)`;
+          */
+          const style = `transform-origin: left center; transform: translate(0%, 0%); width: ${num2_}%;`;
+          this.progressDiv.setAttribute('style', style);
         }
       }
-
       this.elem.innerHTML = num2_;
       this.animationId = this.win.requestAnimationFrame(this.count.bind(this));
     } else {
       this.elem.innerHTML = this.num * 1;
+      this.Running = false;
+      if (this.endFunc !== undefined) {
+        this.endFunc();
+      }
+    }
+  }
+  // 到達点の数字を受け取り、カウントアニメーションを実行する為の事前処理をする関数。
+  counter_core(nm) {
+    if (!this.Running) {
+      this.num = nm * 1;
+      this.startTime = performance.now();
+      this.count(this.startTime);
+      this.Running = true;
+    } else {
+      this.cancelCount();
+    }
+  }
+  // 対象要素の中身の数字を使ってカウントする。
+  counter() {
+    const num = this.elem.innerHTML;
+    this.counter_core(num);
+  }
+  // 要素の数字を基準にすると、意図しない関数の再発動で最終点が変わる可能性がある為、属性から数字を取得する。
+  counter2() {
+    const num = this.elem.getAttribute(this.attr);
+    //const num = this.elem.dataset.num;
+    this.counter_core(num);
+  }
+  // プログレスバーのみ繰り返し動かす場合
+  progress_bar() {
+    const nm = 100;
+    if (!this.Running) {
+      this.num = nm * 1;
+      this.startTime = performance.now();
+      this.progress_bar_count(this.startTime);
+      this.Running = true;
+    } else {
+      this.cancelCount();
+    }
+  }
+
+  // プログレスバーのみ繰り返し動かす場合の実行関数。
+  progress_bar_count(time) {
+    const progress = (time - this.startTime) / this.duration;
+    if (progress < 1) {
+      const num2 = this.num * this.easing(progress);
+
+      let num2_ = Math.round(num2);
+
+      /*
+      「scaleによる伸縮」がdesktop Safariで出来ないので。
+      this.progressDiv.style.transformOrigin = 'left center';
+      this.progressDiv.style.transform = `translate(-50%, -50%) scale(${num2_}%, 100%)`;
+      */
+
+      const style = `transform-origin: left center; transform: translate(0%, 0%); width: ${num2_}%;`;
+      this.progressDiv.setAttribute('style', style);
+
+      this.animationId = this.win.requestAnimationFrame(
+        this.progress_bar_count.bind(this)
+      );
+    } else {
+      this.Running = false;
+      if (this.progress_bar_count_repeat) {
+        this.progressDiv.style.transform = `translate(-50%, -50%) scale(0%, 100%)`;
+        this.progress_bar();
+      }
       if (this.endFunc !== undefined) {
         this.endFunc();
       }
--- js2/set.js
+++ js2/set.js
@@ -3,32 +3,49 @@ import { Counter_Class } from './Counter_class.js';
 const doc = document;
 const win = window;
 
-function easeOutQuad(x) {
-  return 1 - (1 - x) * (1 - x);
-}
-function appear() {
-  const progress_set = doc.querySelector('#progress_set');
-  const loader_coverUp = doc.querySelector('.loader_cover-up');
-  const loader_coverDown = doc.querySelector('.loader_cover-down');
+function Counter_Set() {
+  Counter_Init();
 
-  loader_coverUp.classList.add('coveranime');
-  loader_coverDown.classList.add('coveranime');
-  progress_set.classList.add('fadeOut');
+  // カウント初期化、実行する関数。
+  function Counter_Init() {
+    const elem = doc.querySelector('#splash_text span');
+    const progress_elem = doc.querySelector('#loading_progress');
+    const progress_bar = doc.querySelector('#progress_bar');
+    const Counter = new Counter_Class({
+      elem: elem,
+      duration: 1000,
+      endFunc: appear,
+      easing: easeOutQuad,
+      //progressElem: progress_elem,
+      progressDiv: progress_bar,
+      win: win,
+    });
+    Counter.counter();
+  }
+  // カウント終了後に実行する関数。
+  function appear() {
+    const progress_set = doc.querySelector('#progress_set');
+    const loader_coverUp = doc.querySelector('.loader_cover-up');
+    const loader_coverDown = doc.querySelector('.loader_cover-down');
+    loader_coverDown.addEventListener(
+      'transitionend',
+      () => {
+        progress_set.setAttribute('style', 'display:none;');
+      },
+      {
+        passive: true,
+      }
+    );
+    loader_coverUp.classList.add('coveranime');
+    loader_coverDown.classList.add('coveranime');
+    progress_set.classList.add('fadeOut');
+  }
+  function easeOutQuad(x) {
+    return 1 - (1 - x) * (1 - x);
+  }
 }
 function Init() {
-  const elem = doc.querySelector('#splash_text span');
-  const progress_elem = doc.querySelector('#loading_progress');
-  const progress_bar = doc.querySelector('#progress_bar');
-  const Counter = new Counter_Class({
-    elem: elem,
-    duration: 1000,
-    endFunc: appear,
-    easing: easeOutQuad,
-    progressElem: progress_elem,
-    //progressDiv: progress_bar,
-    win: win,
-  });
-  Counter.counter();
+  Counter_Set();
 }
 
 win.addEventListener('DOMContentLoaded', Init, {
