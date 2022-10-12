b9b01b82bf1b532f412b0e5dfaed770e7e5f1e35
elder-flower <flowercrown10@gmail.com>
2022-10-12 21:52:54 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <p v-pre>{{ message }}</p>
+  <p><a v-bind:href="url">WINGSプロジェクト</a></p>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue';
 export default defineComponent({
   setup() {
-    const message = ref('皆さん、こんにちは！');
-    return { message };
+    const url = ref('https://wings.msn.to/');
+    return { url };
   },
 });
 </script>
