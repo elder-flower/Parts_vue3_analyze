b29775e3af802ef1518584b6cfaf806a1a28245c
elder-flower <flowercrown10@gmail.com>
2022-10-12 21:46:37 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <p>{{ message }}</p>
+  <p v-text="message"></p>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue';
 export default defineComponent({
   setup() {
-    const message = ref('皆さん、こんにちは！3');
+    const message = ref('皆さん、こんにちは！8');
     return { message };
   },
 });
