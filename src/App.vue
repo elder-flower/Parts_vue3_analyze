3454f51cdc3dd8b998a3ba79b8e3beb79de03873
elder-flower <flowercrown10@gmail.com>
2022-10-15 09:32:06 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,17 @@
 <template>
-  <div>
-    <p><a target="_blank" v-bind:href="url">ライフサイクルフック</a></p>
-    <p>
-      <a target="_blank" v-bind:href="url2"
-        >Vue.js(v3)のライフサイクルを確認する！(Composition API)</a
-      >
-    </p>
-  </div>
+  <p>{{ localEmail }}</p>
 </template>
 
 <script>
-import {
-  ref,
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted,
-} from 'vue';
+import { ref, computed } from 'vue';
 export default {
   name: 'App',
   setup() {
-    onBeforeMount(() => {
-      console.log('Component is onBeforeMount!');
+    const email = ref('Y-Suzuki@example.com');
+    const localEmail = computed(() => {
+      return email.value.split('@')[0].toLowerCase();
     });
-    onMounted(() => {
-      console.log('Component is mounted!');
-    });
-    onBeforeUpdate(() => {
-      console.log('Component is onBeforeUpdate!');
-    });
-    onUpdated(() => {
-      console.log('Component is monUpdated!');
-    });
-    onBeforeUnmount(() => {
-      console.log('Component is onBeforeUnmount!');
-    });
-    onUnmounted(() => {
-      console.log('Component is onUnmounted!');
-    });
-
-    const url = ref(
-      'https://v3.ja.vuejs.org/guide/composition-api-lifecycle-hooks.html'
-    );
-    const url2 = ref('https://codelikes.com/vue-lifecycle-composition/');
-    return { url, url2 };
+    return { email, localEmail };
   },
 };
 </script>
