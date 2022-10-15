193f70ab14aebda9a1ec87f77af8762b021e8f77
elder-flower <flowercrown10@gmail.com>
2022-10-15 17:18:49 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,59 @@
 <template>
-  <div>
-    <button v-on:click="onclick">クリック</button>
+  <div id="main" v-on:mousemove="onmousemove">
+    screen: {{ screenX }}/{{ screenY }}<br />
+    page: {{ pageX }}/{{ pageY }}<br />
+    client: {{ clientX }}/{{ clientY }}<br />
+    offset: {{ offsetX }}/{{ offsetY }}
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   name: 'App',
   setup() {
-    onclick = (e) => {
-      console.log(e);
-    };
+    const screenX = ref(0);
+    const screenY = ref(0);
+    const pageX = ref(0);
+    const pageY = ref(0);
+    const clientX = ref(0);
+    const clientY = ref(0);
+    const offsetX = ref(0);
+    const offsetY = ref(0);
 
-    return { onclick };
+    const onmousemove = (e) => {
+      screenX.value = e.screenX;
+      screenY.value = e.screenY;
+      pageX.value = e.pageX;
+      pageY.value = e.pageY;
+      clientX.value = e.clientX;
+      clientY.value = e.clientY;
+      offsetX.value = e.offsetX;
+      offsetY.value = e.offsetY;
+    };
+    return {
+      screenX,
+      screenY,
+      pageX,
+      pageY,
+      clientX,
+      clientY,
+      offsetX,
+      offsetY,
+      onmousemove,
+    };
   },
 };
 </script>
 
 <style scoped>
-div {
-  margin: 30px;
-  text-align: center;
+#main {
+  position: absolute;
+  margin: 50px;
+  top: 20px;
+  left: 20px;
+  height: 150px;
+  width: 500px;
+  border: solid 1px #000;
 }
 </style>
