dee34636f8df3206275e5d2bd7d7c232140d3981
elder-flower <flowercrown10@gmail.com>
2022-10-15 09:26:05 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,51 @@
 <template>
   <div>
-    <form>
-      <input type="button" value="クリック" v-on:click="onclick" />
-    </form>
-    <div>算出プロパティ：{{ randomc }}</div>
-    <div>メソッド：{{ randomm() }}</div>
-    <div>現在日時：{{ current }}</div>
+    <p><a target="_blank" v-bind:href="url">ライフサイクルフック</a></p>
+    <p>
+      <a target="_blank" v-bind:href="url2"
+        >Vue.js(v3)のライフサイクルを確認する！(Composition API)</a
+      >
+    </p>
   </div>
 </template>
 
 <script>
-import { ref, computed } from 'vue';
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
 export default {
   name: 'App',
   setup() {
-    let current = ref(new Date().toLocaleString());
-    //let current = new Date().toLocaleString();
-
-    const randomc = computed(() => {
-      return Math.random();
+    onBeforeMount(() => {
+      console.log('Component is onBeforeMount!');
+    });
+    onMounted(() => {
+      console.log('Component is mounted!');
+    });
+    onBeforeUpdate(() => {
+      console.log('Component is onBeforeUpdate!');
+    });
+    onUpdated(() => {
+      console.log('Component is monUpdated!');
+    });
+    onBeforeUnmount(() => {
+      console.log('Component is onBeforeUnmount!');
+    });
+    onUnmounted(() => {
+      console.log('Component is onUnmounted!');
     });
 
-    const onclick = () => {
-      current.value = new Date().toLocaleString();
-    };
-
-    const randomm = () => {
-      return Math.random();
-    };
-
-    return { current, randomc, onclick, randomm };
+    const url = ref(
+      'https://v3.ja.vuejs.org/guide/composition-api-lifecycle-hooks.html'
+    );
+    const url2 = ref('https://codelikes.com/vue-lifecycle-composition/');
+    return { url, url2 };
   },
 };
 </script>
