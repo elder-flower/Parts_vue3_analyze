d2ba7d1165175559aebba0c00667bb7f920afc0c
elder-flower <flowercrown10@gmail.com>
2022-10-16 17:00:02 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <div id="wrapper">
     <form>
       <label for="name">氏名：</label>
-      <input type="text" id="name" v-on:keyup.escape="onclear" v-model="name" />
-      <!-- <input type="text" id="name" v-on:keyup.esc="onclear" v-model="name"/> -->
+
+      <!--vue3では非推奨のため、使えなくなった。-->
+      <input type="text" id="name" v-on:keyup.27="clear" v-model="name" />
     </form>
   </div>
 </template>
@@ -29,18 +30,5 @@ export default {
   margin: 50px auto;
   width: 90%;
   text-align: center;
-}
-div {
-  padding: 1em;
-  margin: 1em;
-}
-#parent {
-  background: #000;
-}
-#my {
-  background: #411;
-}
-#child {
-  background: #141;
 }
 </style>
