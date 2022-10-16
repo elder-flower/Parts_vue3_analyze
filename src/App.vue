5bb7420ccafa5367c3764542567a27f5dbbafd37
elder-flower <flowercrown10@gmail.com>
2022-10-16 17:01:56 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div id="wrapper">
     <form>
       <label for="name">氏名：</label>
-
-      <!--vue3では非推奨のため、使えなくなった。-->
-      <input type="text" id="name" v-on:keyup.27="clear" v-model="name" />
+      <input type="text" id="name" v-on:keyup.ctrl.q="help" v-model="name" />
+      <!-- <input type="text" v-on:keyup.ctrl.exact.q="help" v-model="name" /> -->
     </form>
   </div>
 </template>
@@ -16,11 +15,11 @@ export default {
 
   setup() {
     const name = ref('匿名');
-    const onclear = () => {
-      name.value = '';
+    const help = () => {
+      window.alert('氏名（漢字）を入力してください');
     };
 
-    return { name, onclear };
+    return { name, help };
   },
 };
 </script>
