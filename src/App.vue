c82cde7d46634e1420f4890c047770adbe63f7bb
elder-flower <flowercrown10@gmail.com>
2022-10-16 17:38:04 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="wrapper">
-    <form method="POST" v-on:submit="onsubmit">
-      <label for="email">メールアドレス：</label>
-      <input id="email" name="email" type="email" />
-      <input type="submit" value="送信" />
-    </form>
+    <MyHello />
   </div>
 </template>
 
 <script>
-import { ref, reactive, computed } from 'vue';
+import MyHello from './components/MyHello.vue';
 export default {
   name: 'App',
-
+  components: { MyHello },
+  /*
   setup() {
-    const onsubmit = (e) => {
-      if (!confirm('送信しても良いですか？')) {
-        e.preventDefault();
-        return;
-      }
-    };
+    const name = ref('Vue.js');
 
-    return { onsubmit };
+    return { name };
   },
+  */
 };
 </script>
 
@@ -31,40 +24,5 @@ export default {
   margin: 50px auto;
   width: 90%;
   text-align: center;
-}
-.cxt {
-  position: absolute;
-  z-index: 99;
-  padding: 0px;
-  border-width: 1px;
-  border-style: solid;
-  background-color: #e8e8e8;
-}
-
-.cxt li {
-  padding: 5px 15px 5px 15px;
-  list-style: none;
-  border-bottom-color: #ccc;
-  border-bottom-width: 1px;
-  border-bottom-style: dotted;
-  color: #000;
-  cursor: pointer;
-}
-
-.cxt a {
-  color: #000;
-  text-decoration: none;
-}
-
-.cxt li.sep {
-  border-bottom-width: 2px;
-  border-bottom-style: solid;
-}
-
-#main {
-  position: relative;
-  border: solid 1px #000;
-  height: 300px;
-  width: 400px;
 }
 </style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>こんにちは、{{ name }}！</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'MyHello',
+  setup() {
+    const name = ref('Vue2.js');
+
+    return { name };
+  },
+};
+</script>
+
+<style scoped></style>
