9ed9fc8aa5784de9f65d94859a089fa171148232
elder-flower <flowercrown10@gmail.com>
2022-10-16 16:52:38 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div id="wrapper">
-    <input type="button" value="結果表示" v-on:click.once="onclick" />
-    <p>今日の運勢は{{ result }}点です。</p>
+    <div id="parent" v-on:click="onParentClick">
+      <!-- <div id="parent" v-on:click.capture="onParentClick"> -->
+      親要素
+      <div id="my" v-on:click="onMyClick">
+        <!-- <div id="my" v-on:click.self="onMyClick"> -->
+        <!-- <div id="my" v-on:click.capture="onMyClick"> -->
+        <!-- <div id="my" v-on:click.stop.self="onMyClick"> -->
+        <!-- <div id="my" v-on:click.self.stop="onMyClick"> -->
+        現在要素
+        <div id="child" v-on:click="onChildClick">
+          <!-- <div id="child" v-on:click.stop="onChildClick"> -->
+          <!-- <div id="child" v-on:click.capture="onChildClick"> -->
+          <!-- <div id="child" v-on:click.capture.stop="onChildClick"> -->
+          子要素
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,12 +26,17 @@ export default {
   name: 'App',
 
   setup() {
-    const result = ref('－');
-    const onclick = () => {
-      result.value = Math.floor(Math.random() * 100) + 1;
+    const onParentClick = () => {
+      console.log('#parent run...');
+    };
+    const onMyClick = () => {
+      console.log('#my run...');
+    };
+    const onChildClick = () => {
+      console.log('#child run...');
     };
 
-    return { result, onclick };
+    return { onParentClick, onMyClick, onChildClick };
   },
 };
 </script>
@@ -27,8 +47,17 @@ export default {
   width: 90%;
   text-align: center;
 }
-#main {
-  padding: 100px;
+div {
+  padding: 1em;
+  margin: 1em;
+}
+#parent {
   background: #000;
+}
+#my {
+  background: #411;
+}
+#child {
+  background: #141;
 }
 </style>
