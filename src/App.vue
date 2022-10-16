954bde58ebfac88e8d4b48aaff95c0c988913626
elder-flower <flowercrown10@gmail.com>
2022-10-16 15:33:42 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="wrapper">
-    <span v-for="i in 10" v-bind:key="i">{{ i * 2 }}　</span>
+    <template v-for="s in songs" v-bind:key="s.title">
+      <header>{{ s.title }}</header>
+      <div>{{ s.lyrics }}</div>
+      <footer>{{ s.composer }} 作曲</footer>
+    </template>
   </div>
 </template>
 
@@ -8,6 +12,32 @@
 import { ref, reactive } from 'vue';
 export default {
   name: 'App',
+  setup() {
+    const songs = ref([
+      {
+        title: '赤とんぼ',
+        lyrics: '夕焼け小焼けの赤とんぼ...',
+        composer: '山田耕作',
+      },
+      {
+        title: '荒城の月',
+        lyrics: '春高楼の花の宴 巡る盃影さして...',
+        composer: '瀧廉太郎',
+      },
+      {
+        title: 'どんぐりころころ',
+        lyrics: 'どんぐりころころ どんぶりこ...',
+        composer: '梁田貞',
+      },
+      {
+        title: '七つの子',
+        lyrics: '烏 なぜ啼くの 烏は山に...',
+        composer: '本居長世',
+      },
+    ]);
+
+    return { songs };
+  },
 };
 </script>
 
