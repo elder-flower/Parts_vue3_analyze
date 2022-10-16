675c383f8d8088fb563e92c759debaf4bd788488
elder-flower <flowercrown10@gmail.com>
2022-10-16 15:42:18 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <div id="wrapper">
-    <table class="table">
-      <th>ISBN</th>
-      <th>書名</th>
-      <th>価格</th>
-      <tr v-for="b in expensiveBooks" v-bind:key="b.isbn">
-        <!-- <tr v-for="b in books" v-if="b.price >= 2500"> -->
-        <td>{{ b.isbn }}</td>
-        <td>{{ b.title }}</td>
-        <td>{{ b.price }}円</td>
-      </tr>
-    </table>
+    <form>
+      <input type="button" value="変更" v-on:click="onclick" />
+    </form>
+    <ul>
+      <li v-for="item in list" v-bind:key="item">{{ item }}</li>
+    </ul>
   </div>
 </template>
 
@@ -19,34 +14,13 @@ import { ref, reactive, computed } from 'vue';
 export default {
   name: 'App',
   setup() {
-    const books = ref([
-      {
-        isbn: '978-4-7981-5757-3',
-        title: 'JavaScript逆引きレシピ',
-        price: 2800,
-      },
-      {
-        isbn: '978-4-8399-6644-7',
-        title: 'たのしいラズパイ電子工作ブック',
-        price: 1900,
-      },
-      {
-        isbn: '978-4-7741-9763-0',
-        title: '3ステップでしっかり学ぶ Python入門',
-        price: 2480,
-      },
-      {
-        isbn: '978-4-7981-5382-7',
-        title: '独習C# 新版',
-        price: 3600,
-      },
-    ]);
+    const list = ref(['赤パジャマ', '青パジャマ', '黄パジャマ']);
+    const onclick = () => {
+      list.value[1] = '茶パジャマ';
+      //list.value.splice(1, 1, '茶パジャマ');
+    };
 
-    const expensiveBooks = computed(() => {
-      return books.value.filter((b) => b.price >= 2500);
-    });
-
-    return { books, expensiveBooks };
+    return { list, onclick };
   },
 };
 </script>
