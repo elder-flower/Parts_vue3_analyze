a28adcb926002ee4b8fb964fd2078b054e5bfca7
elder-flower <flowercrown10@gmail.com>
2022-10-23 17:22:56 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <div id="wrapper">
-    <MyParent your-name="鈴木2"></MyParent>
-    <!-- <MyParent your-name="鈴木"></MyParent> -->
+    <NumberInput request_data="menuData"></NumberInput>
   </div>
 </template>
 
 <script>
-import MyParent from './components/MyParent.vue';
+import axios from 'axios';
+import { ref } from 'vue';
+import NumberInput from './components/NumberInput.vue';
 export default {
   name: 'App',
-  components: { MyParent },
-  /*
-  setup() {
-    const name = ref('Vue.js');
+  components: { NumberInput },
 
-    return { name };
+  setup() {
+    let menuData = ref('');
+
+    const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1';
+    const QUERYSTRING = '?token=token123';
+
+    const get = async () => {
+      //const response = await fetch(ROOT_URL);
+      const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`);
+      //console.log(response);
+      const arr = await response.data;
+      //console.log(arr);
+      menuData.value = arr;
+    };
+
+    get();
+
+    return { menuData };
   },
-  */
 };
 </script>
 
---
+++ src/components/NumberInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ data }}</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'NumberInput',
+  props: ['requestData'],
+
+  setup(props) {
+    const data = ref(props.requestData);
+    console.log(data.value);
+
+    return { data };
+  },
+};
+</script>
+
+<style scoped>
+div {
+  margin: 1em;
+}
+</style>
