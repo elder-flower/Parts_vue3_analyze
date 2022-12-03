0f239d5665b8f2ac90ba6ad1d3d1ad18f1b6a993
elder-flower <flowercrown10@gmail.com>
2022-12-03 21:56:15 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,18 @@
             data-href="#info-1"
             aria-haspopup="dialog"
             id="firstview"
+            v-on:click="onMmodalShow"
+          >
+            お知らせを開く
+          </button>
+        </li>
+        <li>
+          <button
+            class="modal_link"
+            data-href="#info-2"
+            aria-haspopup="dialog"
+            id="firstview"
+            v-on:click="onMmodalShow"
           >
             お知らせを開く
           </button>
@@ -21,36 +33,12 @@
             data-href="#gallery-2"
             aria-haspopup="dialog"
             id="firstview"
+            v-on:click="onMmodalShow"
           >
             画像リスト
           </button>
         </li>
-        <li>
-          <button
-            class="modal_link"
-            data-href="#info-2"
-            aria-haspopup="dialog"
-            id="firstview"
-          >
-            お知らせを開く
-          </button>
-        </li>
       </ul>
-
-      <div id="info-1" class="hide-area">
-        <section class="bg">
-          <h2>お知らせ1</h2>
-          <p>おしらせの内容が入ります。</p>
-        </section>
-      </div>
-
-      <div id="info-2" class="hide-area">
-        <section class="bg">
-          <h2>お知らせ2</h2>
-          <p>おしらせの内容が入ります。</p>
-        </section>
-      </div>
-
       <!--確認を促すモーダル-->
       <p>
         <button
@@ -147,6 +135,7 @@
           data-href="https://www.youtube.com/embed/tiA2fWvX4XU"
           aria-haspopup="dialog"
           id="firstview"
+          v-on:click="onMmodalShow"
         >
           動画を開く
         </button>
@@ -155,25 +144,53 @@
       <!--/area1-->
     </section>
   </main>
-  <ModaL v-bind:is-modal="isModal">
-    <section>
+
+  <ModaL v-bind:is-modal="isModal" v-on:modal-close="onClose">
+    <section class="bg">
       <h2>お知らせ1</h2>
-      <p>おしらせの内容が入ります。</p>
+      <p>
+        おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。
+      </p>
     </section></ModaL
   >
 </template>
 
 <script>
-import { ref, reactive, computed } from 'vue';
+import { ref, reactive, computed, onBeforeUpdate } from 'vue';
 import ModaL from './components/Modal.vue';
 
 export default {
   name: 'App',
   components: { ModaL },
 
-  setup() {
-    const isModal = ref(true);
-    return { isModal };
+  setup(props, context) {
+    const isModal = ref(false);
+
+    const onMmodalShow = () => {
+      console.log('onMmodalShow');
+      if (isModal.value == false) {
+        isModal.value = true;
+      } else {
+        isModal.value = false;
+      }
+    };
+
+    const onClose = (flag) => {
+      console.log('onClose1');
+      isModal.value = flag;
+    };
+
+    onBeforeUpdate(() => {
+      //html要素に「overflow-y:hidden」を付ける。
+      const html = document.documentElement;
+      if (isModal.value) {
+        html.setAttribute('style', 'overflow-y: hidden;');
+      } else {
+        html.removeAttribute('style');
+      }
+    });
+
+    return { isModal, onMmodalShow, onClose };
   },
 };
 </script>
@@ -182,4 +199,8 @@ export default {
 @import './assets/style/reset_btn.scss';
 @import './assets/style/base.scss';
 @import './assets/style/modal_contents.scss';
+
+#test1 {
+  //height: 1500px;
+}
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,27 +1,55 @@
 <template>
-  <div id="modal" v-show="isModal2">
-    <div id="modal_inner">
-      <div id="dialog">
-        <slot></slot>
+  <teleport to="body">
+    <transition name="modal_fade">
+      <div
+        id="modal"
+        v-bind:class="[{ modalshow: isModal2 }]"
+        v-show="isModal2"
+      >
+        <div id="modal_inner" v-on:click="onClose">
+          <div id="t_margin"></div>
+          <div id="dialog">
+            <slot></slot>
+          </div>
+          <div id="b_margin"></div>
+        </div>
+        <button id="modal_close" v-on:click="onClose"></button>
+        <div id="modal_loading"><div></div></div>
       </div>
-    </div>
-  </div>
+    </transition>
+  </teleport>
 </template>
 
 <script>
-import { ref, reactive, watch } from 'vue';
+import { ref, watchEffect } from 'vue';
 export default {
   name: 'Modal',
   props: ['isModal'],
-  //emits: ['nextPos'],
+  emits: ['modalClose'],
   setup(props, context) {
     const isModal2 = ref(props.isModal);
-    console.log(isModal2.value);
-    return { isModal2 };
+
+    watchEffect(() => {
+      isModal2.value = props.isModal;
+    });
+
+    const onClose = () => {
+      console.log('onClose2');
+      context.emit('modalClose', false);
+      isModal2.value = false;
+    };
+
+    return { isModal2, onClose };
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/reset_btn.scss';
+@import '../assets/style/modal_layout.scss';
+@import '../assets/style/modal_behavior.scss';
+@import '../assets/style/modal_style.scss';
 @import '../assets/style/modal.scss';
+@import '../assets/style/modal_close.scss';
+@import '../assets/style/modal_animation.scss';
 </style>
