56da53698a02e300f41c5a75ef0688e65d11ecc2
elder-flower <flowercrown10@gmail.com>
2022-12-27 20:32:49 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <main>
-    <section class="lists">
-      <section v-for="i in lists" v-bind:key="i">list {{ i }}</section>
-    </section>
     <ul class="nav_dot">
       <li v-for="j in dots" v-bind:key="j">
-        <button v-bind:data-id="j" v-on:click="onDotBtn">●</button>
+        <button v-bind:data-id="j - 1" v-on:click="onDotBtn">●</button>
       </li>
     </ul>
+    <section class="lists">
+      <section v-for="i in lists.data" v-bind:key="i.id">
+        list {{ i.id }}
+      </section>
+    </section>
   </main>
 </template>
 
 <script>
-import { ref, watchEffect } from 'vue';
+import { ref, reactive } from 'vue';
 import Modal from './components/Modal.vue';
 
 export default {
@@ -20,37 +22,75 @@ export default {
   components: { Modal },
 
   setup() {
-    const pos = ref(2);
-    const totalNumber = 50;
-    const displayNumber = 15;
+    // 仮想受信したデータ。
+    const data = [];
 
+    // 50個のダミーデータ。
+    for (let i = 1; i < 51; i++) {
+      data.push({ id: i, txt: `txt${i}` });
+    }
+
+    // 現在表示している「pagination」の位置。
+    const pos = ref(0);
+
+    // 「pagination」の総数。
+    const totalNumber = data.length;
+
+    // 一度に表示する 「pagination」の数。
+    const displayNumber = 9;
+
+    // 分割数を割り出す。
     let divisionNumber = Math.floor(totalNumber / displayNumber);
+
+    // 一度に表示する数から余りを算出。
     const remainder = totalNumber % displayNumber;
 
-    let dots = divisionNumber + 1;
+    // dot navigation の数を算出。
+    let dots = divisionNumber;
 
+    // 余りがある場合は、「dots」の数を1つ増やす。
     if (remainder !== 0) {
       dots = divisionNumber + 1;
     }
 
-    watchEffect(() => {});
+    // 受信したデータから表示するデータを切り出す為の処理。
 
-    /*
-    console.log('divisionNumber');
-    console.log(divisionNumber);
-    console.log(remainder);
-    */
+    // 開始位置。
+    let start = pos.value * displayNumber;
 
+    // 終了位置。
+    let end = start + displayNumber;
+
+    // 受信したデータから表示するデータを切り出す。
+    const init_lists = data.slice(start, end);
+
+    // 表示されるデータの配列。リアクティブにする為、「eactive」でラッピング。
+    let lists = reactive({
+      data: init_lists,
+    });
+
+    // 「dots」ボタンを押した時の実行処理関数。
     const onDotBtn = (e) => {
       if (e instanceof Event) {
         const btn = e.currentTarget;
+
+        // クリックされたdotの位置を取得。
         const btn_id = btn.dataset.id;
-        console.log(btn_id);
+        //console.log(btn_id);
+
         pos.value = Number(btn_id);
+
+        // 新たに表示するデータを切り出す為の処理。
+        // 開始位置。
+        start = pos.value * displayNumber;
+
+        // 終了位置。
+        end = start + displayNumber;
+
+        // 表示されるデータの配列を更新。
+        lists.data = data.slice(start, end);
       }
     };
-
-    const lists = pos.value * displayNumber;
 
     return { lists, dots, onDotBtn };
   },
@@ -60,5 +100,5 @@ export default {
 @import './assets/style/variables.scss';
 @import './assets/style/reset_btn.scss';
 @import './assets/style/base.scss';
-@import './assets/style/paginationl.scss';
+@import './assets/style/pagination.scss';
 </style>
