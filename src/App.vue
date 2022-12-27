53e666db1e95fb4200a0eca14cefe4f0c9928de4
elder-flower <flowercrown10@gmail.com>
2022-12-27 19:53:10 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,217 +1,58 @@
 <template>
   <main>
-    <section>
-      <h2>Area 1</h2>
-
-      <!--テキストを含む一般的なモーダル-->
-      <ul class="info-list">
-        <li>
-          <button
-            class="modal_link"
-            data-href="#info-1"
-            aria-haspopup="dialog"
-            id="firstview"
-            v-on:click="onMmodalShow"
-          >
-            お知らせを開く
-          </button>
-        </li>
-        <li>
-          <button
-            class="modal_link"
-            data-href="#info-2"
-            aria-haspopup="dialog"
-            id="firstview"
-            v-on:click="onMmodalShow"
-          >
-            お知らせを開く
-          </button>
-        </li>
-        <li>
-          <button
-            class="modal_link"
-            data-href="#gallery-2"
-            aria-haspopup="dialog"
-            id="firstview"
-            v-on:click="onMmodalShow"
-          >
-            画像リスト
-          </button>
-        </li>
-      </ul>
-      <!--確認を促すモーダル-->
-      <p>
-        <button
-          class="modal_confirm"
-          data-href="#login"
-          aria-haspopup="dialog"
-          id="firstview"
-        >
-          → 確認画面モーダルリンク
-        </button>
-      </p>
-      <div id="login" class="hide-area">
-        <section class="bg">
-          <form
-            method="get"
-            action=""
-            enctype="multipart/form-data"
-            id="form_login"
-          >
-            <h2>ログイン画面</h2>
-            <p>
-              ログインが必要です。
-              <br />この画面はボタンを押さなければ閉じません。
-            </p>
-            <div class="flex">
-              <button
-                type="button"
-                id="form_login_submit"
-                aria-disabled="false"
-              >
-                送信
-              </button>
-              <button
-                type="button"
-                id="form_login_cancel"
-                aria-disabled="false"
-              >
-                キャンセル
-              </button>
-            </div>
-          </form>
-        </section>
-      </div>
-
-      <!--画像のモーダル-->
-      <ul class="gallery-list">
-        <!--data-groupに同じ名前を入れるとグループ化されて表示-->
-        <li>
-          <a
-            href="/img/01.jpg"
-            class="modal_imgs"
-            data-group="gallery"
-            aria-haspopup="dialog"
-            data-href2="/img/01.jpg"
-            ><img src="/img/01.jpg" alt="画像02" width="100"
-          /></a>
-        </li>
-        <li>
-          <a
-            href="/img/02.jpg"
-            class="modal_imgs"
-            data-group="gallery"
-            aria-haspopup="dialog"
-            data-href2="/img/02.jpg"
-            ><img src="/img/02.jpg" alt="画像01" width="100"
-          /></a>
-        </li>
-        <li>
-          <a
-            href="/img/03.jpg"
-            class="modal_imgs"
-            data-group="gallery"
-            aria-haspopup="dialog"
-            data-href2="/img/03.jpg"
-            >画像03</a
-          >
-        </li>
-        <li>
-          <a
-            href="/img/04.jpg"
-            class="modal_imgs"
-            data-group="gallery"
-            aria-haspopup="dialog"
-            data-href2="/img/04.jpg"
-            >画像04</a
-          >
-        </li>
-      </ul>
-
-      <!--動画のモーダル-->
-      <p>
-        <button
-          class="modal_movie"
-          data-href="https://www.youtube.com/embed/tiA2fWvX4XU"
-          aria-haspopup="dialog"
-          id="firstview"
-          v-on:click="onMmodalShow"
-        >
-          動画を開く
-        </button>
-      </p>
-
-      <!--/area1-->
+    <section class="lists">
+      <section v-for="i in lists" v-bind:key="i">list {{ i }}</section>
     </section>
+    <ul class="nav_dot">
+      <li v-for="j in dots" v-bind:key="j">
+        <button v-bind:data-id="j" v-on:click="onDotBtn">●</button>
+      </li>
+    </ul>
   </main>
-
-  <!-- NOTE:
-  使い方。
-  ・<template v-slot:contents>以下にコンテンツを配置する。
-    モーダルコンテンツ内で「閉じるボタン」を使用する場合は以下を設定する。
-
-    v-on:click="onModalClose(false)"
-
-  -->
-  <ModaL
-    v-bind:is-modal="isModal"
-    v-bind:is-btn="isTopRightBtn"
-    v-on:modal-close="onModalClose"
-  >
-    <template v-slot:contents>
-      <section class="bg">
-        <h2>お知らせ1</h2>
-        <p>
-          おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。おしらせの内容が入ります。
-        </p>
-        <button class="close_btn" v-on:click="onModalClose(false)">
-          close btn
-        </button>
-      </section>
-    </template>
-  </ModaL>
 </template>
 
 <script>
-import { ref, reactive, computed, onBeforeUpdate } from 'vue';
-import ModaL from './components/Modal.vue';
+import { ref, watchEffect } from 'vue';
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
-  components: { ModaL },
+  components: { Modal },
 
   setup() {
-    // モーダルの表示非表示を管理する変数。
-    const isModal = ref(false);
+    const pos = ref(2);
+    const totalNumber = 50;
+    const displayNumber = 15;
 
-    //「true」の場合はモーダルコンテンツ内の右上のボタンの表示をする。
-    const isTopRightBtn = true;
+    let divisionNumber = Math.floor(totalNumber / displayNumber);
+    const remainder = totalNumber % displayNumber;
 
-    // クリックした時にモーダルを表示させる関数。
-    const onMmodalShow = () => {
-      console.log('onMmodalShow');
-      isModal.value = true;
-    };
+    let dots = divisionNumber + 1;
 
-    // クリックした時にモーダルを閉じる関数。
-    const onModalClose = () => {
-      console.log('onModalClose1');
-      isModal.value = false;
-    };
+    if (remainder !== 0) {
+      dots = divisionNumber + 1;
+    }
 
-    // モーダルを表示させた時に縦のスクロールバーを表示させない為の処理をする。
-    onBeforeUpdate(() => {
-      //html要素に「overflow-y:hidden」を付ける。
-      const html = document.documentElement;
+    watchEffect(() => {});
 
-      if (isModal.value) {
-        html.setAttribute('style', 'overflow-y: hidden;');
-      } else {
-        html.removeAttribute('style');
+    /*
+    console.log('divisionNumber');
+    console.log(divisionNumber);
+    console.log(remainder);
+    */
+
+    const onDotBtn = (e) => {
+      if (e instanceof Event) {
+        const btn = e.currentTarget;
+        const btn_id = btn.dataset.id;
+        console.log(btn_id);
+        pos.value = Number(btn_id);
       }
-    });
+    };
 
-    return { isModal, onMmodalShow, onModalClose, isTopRightBtn };
+    const lists = pos.value * displayNumber;
+
+    return { lists, dots, onDotBtn };
   },
 };
 </script>
@@ -219,9 +60,5 @@ export default {
 @import './assets/style/variables.scss';
 @import './assets/style/reset_btn.scss';
 @import './assets/style/base.scss';
-@import './assets/style/modal_contents.scss';
-
-#test1 {
-  //height: 1500px;
-}
+@import './assets/style/paginationl.scss';
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,68 +1,15 @@
-<template>
-  <teleport to="body">
-    <transition name="modal_fade">
-      <div
-        id="modal"
-        v-bind:class="[{ modalshow: isModal2 }]"
-        v-show="isModal2"
-      >
-        <div id="modal_inner" v-on:click="onModalClose">
-          <div id="t_margin"></div>
-          <div id="dialog" v-on:click.stop="onStopClick">
-            <slot name="contents"></slot>
-          </div>
-          <div id="b_margin"></div>
-        </div>
-        <button
-          id="modal_close"
-          v-on:click="onModalClose"
-          v-if="isBtn2"
-        ></button>
-        <div id="modal_loading"><div></div></div>
-      </div>
-    </transition>
-  </teleport>
-</template>
+<template></template>
 
 <script>
 import { ref, watchEffect } from 'vue';
 export default {
   name: 'Modal',
-  props: ['isModal', 'isBtn'],
-  emits: ['modalClose'],
+  props: [''],
+  emits: [''],
   setup(props, context) {
-    // モーダルの表示非表示を管理する変数。
-    const isModal2 = ref(props.isModal);
-
-    // 右上に表示される」「閉じるボタン」の表示非表示を管理する変数。
-    const isBtn2 = props.isBtn;
-
-    watchEffect(() => {
-      isModal2.value = props.isModal;
-    });
-
-    //「閉じるボタン」等が押された時に親へ「emit」を送る関数。
-    const onModalClose = () => {
-      console.log('onModalClose2');
-      context.emit('modalClose', false);
-      isModal2.value = false;
-    };
-
-    // コンテンツ部分をクリックした時にイベントを伝播させないようにする。
-    // 「Event.stopPropagation()」=「v-on:click.stop」
-    const onStopClick = () => {};
-
-    return { isModal2, onModalClose, onStopClick, isBtn2 };
+    return {};
   },
 };
 </script>
 
-<style lang="scss" scoped>
-@import '../assets/style/reset_btn.scss';
-@import '../assets/style/modal_layout.scss';
-@import '../assets/style/modal_behavior.scss';
-@import '../assets/style/modal_style.scss';
-@import '../assets/style/modal.scss';
-@import '../assets/style/modal_close.scss';
-@import '../assets/style/modal_animation.scss';
-</style>
+<style lang="scss" scoped></style>
